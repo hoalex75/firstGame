575918d8c874b9ff5e3c77fb7ec846ebc0e1c55c
hoalex <svt75@hotmail.com>
2018-10-17 00:29:34 +0200

    All the first version of classes has been created with corresponding methods.

--- firstGame/Personnage.swift
+++ firstGame/Personnage.swift
@@ -11,9 +11,10 @@ import Foundation
 class Personnage{
     static var names = Set<String>()
     var name = ""
+    var maxLifePoints = 0
     var lifePoints = 0
     
-    
+    //Builder
     init(name : String){
         if Personnage.existingName(nameWeWant: name){
         } else {
@@ -21,12 +22,88 @@ class Personnage{
             self.name = name
         }
     }
-    
+    //Methods
     static func existingName(nameWeWant : String) -> Bool {
         return names.contains(nameWeWant)
     }
     
     func displayName(){
         print("My name is \(name)")
+    }
+}
+//class Warrior inherited from Personnage, an abstract class from warriors' subclasses
+class Warrior : Personnage{
+    var attackValue = 0
+    
+    //Methods
+    func attackTarget(target : Personnage){
+        target.lifePoints = target.lifePoints - attackValue
+        print("Life points remaining on \(target.name) are of \(target.lifePoints) ")
+    }
+}
+
+//class Healer inherited from Personnage, an abstract class from healers' subclasses
+class Healer : Personnage{
+    var healValue = 0
+    
+    //Methods
+    private func isFullyRestore(target : Personnage) -> Bool{
+        if target.lifePoints + healValue >= target.maxLifePoints{
+            return true
+        }else {
+            return false
+        }
+    }
+    
+    func healTarget(target : Personnage){
+        if isFullyRestore(target: target){
+            target.lifePoints = target.maxLifePoints
+        }
+        else {
+            target.lifePoints = target.lifePoints + healValue
+        }
+        print("Life points remaining on \(target.name) are of \(target.lifePoints) ")
+    }
+}
+
+//Real classes
+class Mage : Healer {
+    
+    //Builder calling builder from Personnage
+    override init(name: String) {
+        super.init(name: name)
+        healValue = 5
+        maxLifePoints = 80
+        lifePoints = maxLifePoints
+    }
+}
+
+class Combattant : Warrior {
+    //Builder calling builder from Personnage
+    override init(name: String) {
+        super.init(name: name)
+        attackValue = 10
+        maxLifePoints = 100
+        lifePoints = maxLifePoints
+    }
+}
+
+class Colosse : Warrior {
+    //Builder calling builder from Personnage
+    override init(name: String) {
+        super.init(name: name)
+        attackValue = 6
+        maxLifePoints = 140
+        lifePoints = maxLifePoints
+    }
+}
+
+class Nain : Warrior {
+    //Builder calling builder from Personnage
+    override init(name: String) {
+        super.init(name: name)
+        attackValue = 14
+        maxLifePoints = 60
+        lifePoints = maxLifePoints
     }
 }
