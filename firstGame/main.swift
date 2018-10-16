7ea57207f227765af51a00a9a6c19d454b89b0dc
hoalex <svt75@hotmail.com>
2018-10-16 23:00:55 +0200

    Initial Commit

---
+++ firstGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  firstGame
+//
+//  Created by Alex on 16/10/2018.
+//  Copyright © 2018 Alexandre Holet. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
